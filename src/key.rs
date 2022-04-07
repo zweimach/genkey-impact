8efa1817a5d8833657c4a4cbf4d766143df70b02
Ananda Umamil <zweimach@zweimach.org>
2022-04-08 02:07:14 +0700

    Initial commit

---
+++ src/key.rs
@@ -0,0 +1,78 @@
+use openssl::asn1::Asn1Time;
+use openssl::hash::MessageDigest;
+use openssl::nid::Nid;
+use openssl::pkcs12::Pkcs12;
+use openssl::pkey::PKey;
+use openssl::rsa::Rsa;
+use openssl::x509::{X509Name, X509};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Key {
+    #[serde(rename = "npwp")]
+    tax_id: String,
+    company_name: String,
+    email: String,
+    password: String,
+    location: Option<String>,
+    province: Option<String>,
+    validity: Option<u32>,
+}
+
+impl Key {
+    pub fn to_pkcs12(&self) -> Vec<u8> {
+        let rsa = Rsa::generate(2048).expect("Generate RSA key failed");
+        let pkey = PKey::from_rsa(rsa).expect("Generate key pair failed");
+
+        let mut builder = X509Name::builder().expect("Get X509Name builder failed");
+        let tax_id = self.tax_id.replace('.', "").replace('-', "");
+        let timestamp = "202109241029Z"; // TODO
+        let common_name = format!("{}-{}-{}", self.company_name, timestamp, tax_id);
+        builder
+            .append_entry_by_text("CN", &common_name)
+            .expect("Set X509Name `CN` failed");
+        let organizational_unit = "Jakarta"; // TODO
+        builder
+            .append_entry_by_text("OU", organizational_unit)
+            .expect("Set X509Name `OU` failed");
+        let organization = "OP"; // TODO
+        builder
+            .append_entry_by_text("O", organization)
+            .expect("Set X509Name `O` failed");
+        let location = "Jakarta".to_string();
+        builder
+            .append_entry_by_text("L", &self.location.clone().unwrap_or(location))
+            .expect("Set X509Name `L` failed");
+        let state = "DKI Jakarta".to_string();
+        builder
+            .append_entry_by_text("ST", &self.province.clone().unwrap_or(state))
+            .expect("Set X509Name `ST` failed");
+        let country = "ID"; // TODO
+        builder
+            .append_entry_by_text("C", country)
+            .expect("Set X509Name `C` failed");
+        builder
+            .append_entry_by_nid(Nid::PKCS9_EMAILADDRESS, &self.email)
+            .expect("Set X509Name `EMAILADDRESS` failed");
+        let name = builder.build();
+
+        let mut builder = X509::builder().expect("Get X509 builder failed");
+        builder.set_version(2).expect("Set X509 version failed");
+        builder.set_subject_name(&name).expect("Set X509 subject name failed");
+        builder.set_issuer_name(&name).expect("Set X509 issuer name failed");
+        builder.set_pubkey(&pkey).expect("Set X509 public key failed");
+        let not_before = Asn1Time::from_str(timestamp).expect("Set Asn1Time before failed");
+        builder.set_not_before(&not_before).expect("Set X509 start time failed");
+        let not_after = Asn1Time::days_from_now(365).expect("Set Asn1Time after failed");
+        builder.set_not_after(&not_after).expect("Set X509 validity failed");
+        builder.sign(&pkey, MessageDigest::sha256()).expect("Sign X509 failed");
+        let certificate: X509 = builder.build();
+
+        let builder = Pkcs12::builder();
+        let pkcs12 = builder
+            .build(&self.password, &tax_id, &pkey, &certificate)
+            .expect("Generate Pkcs12 failed");
+        pkcs12.to_der().expect("Serialize Pkcs12 to DER failed")
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,30 @@
+mod key;
+
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use std::env;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let port = env::var("GENKEY_PORT")
+        .unwrap_or_else(|_| "8080".to_string())
+        .parse::<u16>()
+        .expect("Failed to parse `GENKEY_PORT`");
+    let host = "0.0.0.0";
+    println!("Listening at {}:{}", host, port);
+    HttpServer::new(|| App::new().service(status).service(generate_key))
+        .bind((host, port))?
+        .run()
+        .await
+}
+
+#[get("/status")]
+async fn status() -> impl Responder {
+    "OK"
+}
+
+#[post("/pkcs12")]
+async fn generate_key(key: web::Json<key::Key>) -> HttpResponse {
+    HttpResponse::Ok()
+        .content_type("application/x-pkcs12")
+        .body(key.to_pkcs12())
+}

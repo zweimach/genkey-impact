9d08bd9733d4610e75dd561bea1f8fd617414db1
Ananda Umamil <zweimach@zweimach.org>
2022-04-08 14:59:03 +0700

    feat: x509 serial number & subject key id extension

--- src/key.rs
+++ src/key.rs
@@ -1,11 +1,15 @@
-use openssl::asn1::Asn1Time;
+use openssl::asn1::{Asn1Integer, Asn1Time};
+use openssl::bn::BigNum;
 use openssl::hash::MessageDigest;
 use openssl::nid::Nid;
 use openssl::pkcs12::Pkcs12;
 use openssl::pkey::PKey;
 use openssl::rsa::Rsa;
+use openssl::x509::extension::SubjectKeyIdentifier;
 use openssl::x509::{X509Name, X509};
+use rand::Rng;
 use serde::{Deserialize, Serialize};
+
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
@@ -22,12 +26,14 @@ pub struct Key {
 
 impl Key {
     pub fn to_pkcs12(&self) -> Vec<u8> {
+        let mut rng = rand::thread_rng();
+
         let rsa = Rsa::generate(2048).expect("Generate RSA key failed");
         let pkey = PKey::from_rsa(rsa).expect("Generate key pair failed");
 
         let mut builder = X509Name::builder().expect("Get X509Name builder failed");
         let tax_id = self.tax_id.replace('.', "").replace('-', "");
-        let timestamp = "202109241029Z"; // TODO
+        let timestamp = "202109241029"; // TODO
         let common_name = format!("{}-{}-{}", self.company_name, timestamp, tax_id);
         builder
             .append_entry_by_text("CN", &common_name)
@@ -62,11 +68,22 @@ impl Key {
         builder.set_subject_name(&name).expect("Set X509 subject name failed");
         builder.set_issuer_name(&name).expect("Set X509 issuer name failed");
         builder.set_pubkey(&pkey).expect("Set X509 public key failed");
-        let not_before = Asn1Time::from_str(timestamp).expect("Set Asn1Time before failed");
+        let not_before = Asn1Time::from_str(&format!("{}Z", timestamp)).expect("Set Asn1Time before failed");
         builder.set_not_before(&not_before).expect("Set X509 start time failed");
         let not_after = Asn1Time::days_from_now(365).expect("Set Asn1Time after failed");
         builder.set_not_after(&not_after).expect("Set X509 validity failed");
         builder.sign(&pkey, MessageDigest::sha256()).expect("Sign X509 failed");
+        let subject_key_id = SubjectKeyIdentifier::new()
+            .build(&builder.x509v3_context(None, None))
+            .expect("Generate SubjectKeyIdentifier failed");
+        builder
+            .append_extension(subject_key_id)
+            .expect("Set X509 extension failed");
+        let random_serial = &BigNum::from_u32(rng.gen()).expect("Generate BigNum failed");
+        let serial_number = Asn1Integer::from_bn(random_serial).expect("Generate Asn1Integer failed");
+        builder
+            .set_serial_number(&serial_number)
+            .expect("Set X509 serial number failed");
         let certificate: X509 = builder.build();
 
         let builder = Pkcs12::builder();
